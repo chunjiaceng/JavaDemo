bed55c8cb74e3029cc5ac8ad8ebe3e0376c6b981
Ruby Ceng <rubyceng0326@gmail.com>
2024-09-19 22:29:42 +0800

    测试

--- web/demo/vue.config.js
+++
@@ -1,8 +0,0 @@
-const { defineConfig } = require("@vue/cli-service");
-module.exports = defineConfig({
-  transpileDependencies: true,
-  lintOnSave: false,
-  devServer: {
-    port: 8000, // 你想要设置的端口号
-  },
-});
